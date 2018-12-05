67eab7bcdd90f1e3fb1786a2d32d9adeee57697b
Pascal Thomet <pthomet@gmail.com>
2018-12-05 16:32:26 +0100

    Readme / Exemple cling with shell

---
+++ init.cpp
@@ -0,0 +1,24 @@
+// Load this file into cling, like so:
+//
+// > cling --std=c++14
+// [cling]$ .L init.cpp          <=== Type this to load this file
+
+#pragma cling add_library_path("/usr/local/lib")
+#pragma cling add_include_path("/usr/local/include/opencv4")
+
+#pragma cling load("libopencv_core.so")
+#pragma cling load("libopencv_calib3d.so")
+#pragma cling load("libopencv_highgui.so")
+#pragma cling load("libopencv_imgcodecs.so")
+#pragma cling load("libopencv_imgproc.so")
+
+//#pragma cling add_include_path("fplus_include/")
+#pragma cling add_include_path(".")
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <fplus/fplus.hpp>
