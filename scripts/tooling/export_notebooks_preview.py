889fe774cba4d529bf608ae8872de5e822ea000d
Pascal Thomet <pthomet@gmail.com>
2018-12-06 21:07:25 +0100

    improve nav

--- scripts/tooling/export_notebooks_preview.py
+++ scripts/tooling/export_notebooks_preview.py
@@ -2,6 +2,8 @@
 import os
 import os.path
 import subprocess
+import jupyter
+import nbformat
 
 os.environ["PATH"] = os.environ["PATH"] + os.pathsep + "/srv/conda/bin"
 
@@ -22,6 +24,9 @@ def file_with_extension(extension:str, directory:str, add_directory:bool = True)
 def find_notebooks():
   return file_with_extension(".ipynb", NOTEBOOKS_DIR)
 
+def find_md():
+  return file_with_extension(".md", NOTEBOOKS_DIR)
+
 
 def make_reveal_js_slideshow(notebook_file):
   base_cmd = "jupyter nbconvert --to slides {} --reveal-prefix=reveal.js --SlidesExporter.reveal_theme=serif --SlidesExporter.reveal_scroll=True"
@@ -41,5 +46,28 @@ def make_all_slideshows():
     make_html_preview(f)
 
 
+def write_markdown_as_notebook(markdown_code: str, out_ipynb_filename):
+  nb = nbformat.v4.new_notebook()
+  nb['cells'] = [nbformat.v4.new_markdown_cell(markdown_code)]
+  with open(out_ipynb_filename, 'w') as f:
+      nbformat.write(nb, f)
+
+
+def make_one_markdown_preview(notebook_file):
+  with open(notebook_file) as f:
+    markdown_code = f.read()
+  tmp_notebook = notebook_file + ".ipynb"
+  write_markdown_as_notebook(markdown_code, tmp_notebook)
+  make_html_preview(tmp_notebook)
+  os.remove(tmp_notebook)
+
+
+def make_all_md_previews():
+  for f in find_md():
+    print("Making Markdown previews for {}".format(f))
+    make_one_markdown_preview(f)
+
+
 if __name__ == "__main__":
   make_all_slideshows()
+  make_all_md_previews()
