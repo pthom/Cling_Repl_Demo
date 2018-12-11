be1d6790ab50dc4d44b58b831187a4c6cda5fdb0
Pascal Thomet <pthomet@gmail.com>
2018-12-11 02:11:32 +0100

    remove unused documents

--- scripts/tooling/servers.sh
+++
@@ -1,4 +0,0 @@
-DIR="$(dirname "$(greadlink -f "$0")")" # readlink on linux (brew install coreutils on mac)
-MAINDIR=$DIR/../..
-$MAINDIR/scripts/html_output_server.sh &
-$MAINDIR/scripts/docker_run_notebook.sh &
