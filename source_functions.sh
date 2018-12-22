f907993540408b16aeb4517f2c15acfa4729e679
Pascal Thomet <pthomet@gmail.com>
2018-12-22 14:24:58 +0100

    update cleantype and functions

--- source_functions.sh
+++ source_functions.sh
@@ -21,29 +21,29 @@ function zz_docker_login_notebook() {
 }
 
 
-function zz_tn_git_push() {
-    cd external/type_name
+function zz_ct_git_push() {
+    cd external/cleantype
     git push
     cd -
     git push
 }
 
-function zz_tn_commit_manual() {
+function zz_ct_commit_manual() {
     if [ "$#" -ne 1 ]; then
         echo "Need a commit message"
         return
     fi
     commit_msg=$1
 
-    cd external/type_name
-    git add README.md notebooks/typename/typename.ipynb
+    cd external/cleantype
+    git add README.md notebooks/cleantype/cleantype.ipynb
     git commit -m "$commit_msg (docs)"
     cd -
-    git add external/type_name
+    git add external/cleantype
     git commit -m "update submodule external/ $commit_msg"
 }
 
-function zz_tn_manual_update_and_push() {
+function zz_ct_manual_update_and_push() {
     if [ "$#" -ne 1 ]; then
         echo "Need a commit message"
         return
@@ -51,10 +51,10 @@ function zz_tn_manual_update_and_push() {
     commit_msg=$1
 
     zz_docker_export_notebooks_preview
-    cp html_output/external/type_name/notebooks/typename/typename.md external/type_name/README.md
+    cp html_output/external/cleantype/notebooks/cleantype/cleantype.md external/cleantype/README.md
 
-    zz_tn_commit_manual $commit_msg
-    zz_tn_git_push
+    zz_ct_commit_manual $commit_msg
+    zz_ct_git_push
 }
 
 function zz_html_output_server() {
